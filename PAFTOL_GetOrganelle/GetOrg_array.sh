2396a59b98dfc5fe45511f3e325506ff45190a70
Paul Bailey <P.Bailey@kew.org>
2023-01-16 10:21:58 +0000

    Fixed all calls to get_organelle_from_reads.py to use --overwrite flag

--- PAFTOL_GetOrganelle/GetOrg_array.sh
+++ PAFTOL_GetOrganelle/GetOrg_array.sh
@@ -33,12 +33,13 @@ if [ ! -z "$file_path_R2" ]; then
 	
 	if [ $org == pt ] 
 	then
-		get_organelle_from_reads.py -1 Data/$file_R1.gz -2 Data/$file_R2.gz -o GetOrg/"$sample"_pt \
+		# Paul B. - added '--overwrite 'otherwise get organelle will not run if folder already exists  
+		get_organelle_from_reads.py --overwrite -1 Data/$file_R1.gz -2 Data/$file_R2.gz -o GetOrg/"$sample"_pt \
 		--max-reads 536870912 -R 20 -k 21,45,65,85,105 -t $ncpu -F embplant_pt --zip-files > \
 		logs/log_${sample}_pt.log 2> logs/log_${sample}_pt.err
 	elif [ $org == nr ]
 	then
-		 get_organelle_from_reads.py -1 Data/$file_R1.gz -2 Data/$file_R2.gz -o GetOrg/"$sample"_nr \
+		 get_organelle_from_reads.py --overwrite -1 Data/$file_R1.gz -2 Data/$file_R2.gz -o GetOrg/"$sample"_nr \
 		--max-reads 536870912 -R 10 -k 35,85,115 -t $ncpu -F embplant_nr --zip-files > \
 		logs/log_${sample}_nr.log 2> logs/log_${sample}_nr.err
 	fi
