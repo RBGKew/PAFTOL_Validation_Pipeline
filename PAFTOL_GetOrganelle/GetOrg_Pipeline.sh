85162708784fa4965ed91d0d29917a9cd718757e
Paul Bailey <P.Bailey@kew.org>
2025-04-16 16:26:21 +0100

    Made changes to use original fastq files and create sample list manually

--- PAFTOL_GetOrganelle/GetOrg_Pipeline.sh
+++ PAFTOL_GetOrganelle/GetOrg_Pipeline.sh
@@ -1,69 +1,86 @@
 #!/bin/bash
-module load python/3
+# Paul B. removed: module load python/3
 
 # Only arguments needed are DataSource and paftol_export
 # e.g: ./GetOrg_Pipeline.sh 2021-07-27_paftol_export.csv "PAFTOL"
+
+### Paul B. modified usage to:
+### Working dir: paftol/Organelles[_PAFTOL2.0]
+### ./GetOrg_Pipeline.sh <paftol_export_file>  <datasource> <path_to_all_fastq_files> <sample_list>
+### Example: ./GetOrg_Pipeline.sh 2021-07-27_paftol_export.csv  PAFTOL  \
+###          /mnt/shared/projects/rbgk/projects/paftol/AllData_symlinks_PAFTOL2.0  sample_list.csv
+### Format for <sample_list>: idSequence,R1_fastq_file_name,R2_fastq_file_name
+###
+### Note: there is now only one sample list controlling which "pt" and "nr" runs are done - hope that's OK
+
+
+paftol_export=$1	# Paul B.: required by GetOrg_prep.py only
 DataSource=$2
-paftol_export=$1
+fastqFilePath=$3	# Paul B. added: replaces 'Data' folder in GetOrg_array.sh lines ~37 and ~42
+sampleList=$4		# Paul B. added: for manually creating list of samples to run and their fastq file names 		
 rem_search="fasta" # log or fasta
 slurmThrottle=5
 
 
 ## Make lists of remaining  samples that have no organelles recovered
 ### Paul B. - can remove the next 3 lines for tests and prpare the remaining_*.txt fime manually 
-rm -f $DataSource/remaining_pt.txt
-rm -f $DataSource/remaining_nr.txt;
-python GetOrg_prep.py --db $paftol_export --DataSource $DataSource --rem_search $rem_search
+# Paul B. removed: rm -f $DataSource/remaining_pt.txt
+# Paul B. removed: rm -f $DataSource/remaining_nr.txt;
+# Paul B. removed: python GetOrg_prep.py --db $paftol_export --DataSource $DataSource --rem_search $rem_search
 
 
 ## Go to dir and create working directories
+mkdir -p $DataSource	# Paul B. added
 cd $DataSource
 mkdir -p GetOrg; mkdir -p logs; mkdir -p fasta_pt; mkdir -p fasta_nr; mkdir -p Archives;
 
 
+### Paul B. removed copying of fastq files - will use files/symlinks from the existing areas
 ## Copy .fastq.gz files in currrent Data directory
-mkdir -p Data;
-while read iline; do
-	file_path_R1="$(cut -d',' -f2 <<<"$iline")"
-	file_R1=`basename "$file_path_R1"`; file_R1=${file_R1/.gz/}
-	echo $file_R1
-	cp -n $file_path_R1 Data/$file_R1.gz
+# mkdir -p Data;
+# while read iline; do
+# 	file_path_R1="$(cut -d',' -f2 <<<"$iline")"
+# 	file_R1=`basename "$file_path_R1"`; file_R1=${file_R1/.gz/}
+# 	echo $file_R1
+# 	cp -n $file_path_R1 Data/$file_R1.gz
 
-	file_path_R2="$(cut -d',' -f3 <<<"$iline")"
-	if [ ! -z "$file_path_R2" ]; then
-		file_R2=`basename "$file_path_R2"`; file_R2=${file_R2/.gz/}
-		echo $file_R2
-		cp -n $file_path_R2 Data/$file_R2.gz
-	else
-		echo "No R2"
-	fi
-done < remaining_pt.txt
+# 	file_path_R2="$(cut -d',' -f3 <<<"$iline")"
+# 	if [ ! -z "$file_path_R2" ]; then
+# 		file_R2=`basename "$file_path_R2"`; file_R2=${file_R2/.gz/}
+# 		echo $file_R2
+# 		cp -n $file_path_R2 Data/$file_R2.gz
+# 	else
+# 		echo "No R2"
+# 	fi
+# done < remaining_pt.txt
 
-while read iline; do
-	file_path_R1="$(cut -d',' -f2 <<<"$iline")"
-	file_R1=`basename "$file_path_R1"`; file_R1=${file_R1/.gz/}
-	echo $file_R1
-	cp -n $file_path_R1 Data/$file_R1.gz
+# while read iline; do
+# 	file_path_R1="$(cut -d',' -f2 <<<"$iline")"
+# 	file_R1=`basename "$file_path_R1"`; file_R1=${file_R1/.gz/}
+# 	echo $file_R1
+# 	cp -n $file_path_R1 Data/$file_R1.gz
 
-	file_path_R2="$(cut -d',' -f3 <<<"$iline")"
-	if [ ! -z "$file_path_R2" ]; then
-		file_R2=`basename "$file_path_R2"`; file_R2=${file_R2/.gz/}
-		echo $file_R2
-		cp -n $file_path_R2 Data/$file_R2.gz
-	else
-		echo "No R2"
-	fi
-done < remaining_nr.txt
+# 	file_path_R2="$(cut -d',' -f3 <<<"$iline")"
+# 	if [ ! -z "$file_path_R2" ]; then
+# 		file_R2=`basename "$file_path_R2"`; file_R2=${file_R2/.gz/}
+# 		echo $file_R2
+# 		cp -n $file_path_R2 Data/$file_R2.gz
+# 	else
+# 		echo "No R2"
+# 	fi
+# done < remaining_nr.txt
 
 
 ## Launch remaining pt
 a=($(wc 'remaining_pt.txt')); Ns_pt=${a[0]}; echo $Ns_pt
 if (( $Ns_pt > 0 )); then
-	sbatch --array=1-${Ns_pt}%$slurmThrottle ../GetOrg_array.sh remaining_pt.txt "pt"
+	### Paul B changed: sbatch --array=1-${Ns_pt}%$slurmThrottle ../GetOrg_array.sh remaining_pt.txt "pt"
+	sbatch --array=1-${Ns_pt}%$slurmThrottle ../GetOrg_array.sh ../${sampleList} "pt"
 fi
 
 ## Launch remaining nr
 a=($(wc 'remaining_nr.txt')); Ns_nr=${a[0]}; echo $Ns_nr
 if (( $Ns_nr > 0 )); then
-	sbatch --array=1-${Ns_nr}%$slurmThrottle ../GetOrg_array.sh remaining_nr.txt "nr"
+	### Paul B changed: sbatch --array=1-${Ns_nr}%$slurmThrottle ../GetOrg_array.sh remaining_nr.txt "nr"
+	sbatch --array=1-${Ns_nr}%$slurmThrottle ../GetOrg_array.sh ../${sampleList} "nr"
 fi
--- PAFTOL_GetOrganelle/GetOrg_array.sh
+++ PAFTOL_GetOrganelle/GetOrg_array.sh
@@ -8,13 +8,14 @@
 #SBATCH --ntasks=1
 ncpu=4
 
-module load python/3
-module load blast bowtie2 spades
-module load getorganelle
+# Paul B. removed: module load python/3
+# Paul B. removed: module load blast bowtie2 spades
+# Paul B. removed: module load getorganelle
 
 
 sample_file=$1
 org=$2
+fastqFilePath=$3 # Paul B. added: full path to files, replaces the 'Data' location
 
 iline=$(sed -n "$SLURM_ARRAY_TASK_ID"p $sample_file)
 echo $iline
@@ -27,19 +28,24 @@ echo $file_R1
 
 file_path_R2="$(cut -d',' -f3 <<<"$iline")"
 
-if [ ! -z "$file_path_R2" ]; then
+### Paul B. changed: if [ ! -z "$file_path_R2" ]; then - changed to -s to test whether R2 file exists, if not data is assumed to be single end.
+###                  (applies to small amount of SRA data only to date) 
+if [ -s "$file_path_R2" ]; then
 	file_R2=`basename "$file_path_R2"`; file_R2=${file_R2/.gz/}
 	echo $file_R2
 	
 	if [ $org == pt ] 
 	then
-		# Paul B. - added '--overwrite 'otherwise get organelle will not run if folder already exists  
-		get_organelle_from_reads.py --overwrite -1 Data/$file_R1.gz -2 Data/$file_R2.gz -o GetOrg/"$sample"_pt \
+		### Paul B. - added '--overwrite 'otherwise get organelle will not run if folder already exists
+		### Paul B. - replaced Data/ with $fastqFilePath in all 6 places below to make the file location more flexible 
+		### Paul B. changed: get_organelle_from_reads.py --overwrite -1 Data/$file_R1.gz -2 Data/$file_R2.gz -o GetOrg/"$sample"_pt \
+		get_organelle_from_reads.py --overwrite -1 $fastqFilePath/$file_R1.gz -2 $fastqFilePath/$file_R2.gz -o GetOrg/"$sample"_pt \
 		--max-reads 536870912 -R 20 -k 21,45,65,85,105 -t $ncpu -F embplant_pt --zip-files > \
 		logs/log_${sample}_pt.log 2> logs/log_${sample}_pt.err
 	elif [ $org == nr ]
 	then
-		 get_organelle_from_reads.py --overwrite -1 Data/$file_R1.gz -2 Data/$file_R2.gz -o GetOrg/"$sample"_nr \
+		### Paul B. changed: get_organelle_from_reads.py --overwrite -1 Data/$file_R1.gz -2 Data/$file_R2.gz -o GetOrg/"$sample"_nr \
+		get_organelle_from_reads.py --overwrite -1 $fastqFilePath/$file_R1.gz -2 $fastqFilePath/$file_R2.gz -o GetOrg/"$sample"_nr \
 		--max-reads 536870912 -R 10 -k 35,85,115 -t $ncpu -F embplant_nr --zip-files > \
 		logs/log_${sample}_nr.log 2> logs/log_${sample}_nr.err
 	fi
