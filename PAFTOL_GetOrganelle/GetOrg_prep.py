7ec3964b8c4933960746d7e45ec0d26b437cdf67
Berta Gallego <b.gallego@kew.org>
2025-08-05 15:26:35 +0100

    [GetOrg] Use given path to fastq files
    The base path is used to create the paths for each sample based on the
    data source. This is useful to address a batch of data under the same
    location or when symlinks in the same directory are used to point to the
    source files in several other directories.

--- PAFTOL_GetOrganelle/GetOrg_prep.py
+++ PAFTOL_GetOrganelle/GetOrg_prep.py
@@ -7,15 +7,11 @@
 # Copyright © 2020 The Board of Trustees of the Royal Botanic Gardens, Kew
 ##################################
 
-# In[1]:
-
-
 import pandas as pd
 import os; import sys
 import argparse
+from pathlib import Path
 
-
-# In[2]:
 
 def main():
     ## Parameters
@@ -45,29 +41,26 @@ def main():
     print("Done: prepared input files with accessions for recovery pipeline.")
 
 
-def add_fastq_files_paths(db, DataSource):
+def add_fastq_files_paths(db, DataSource, src_path):
+    """
+    Add Sample_Name, R1_path, and R2_path columns to db based on the DataSource.
+    DataSource can be 'PAFTOL', 'GAP', or 'SRA'.
+    """
+    fastq_path = src_path
     if DataSource == 'PAFTOL':
-        # Paul B. - modified path to process PAFTOL2.0 data
-        #fastq_path = '/science/projects/paftol/AllData_symlinks/'
-        fastq_path = '/science/projects/paftol/AllData_symlinks_PAFTOL2.0/'
-        db['Sample_Name'] = 'PAFTOL_' + db['idSequencing'].astype(int).astype('str').str.zfill(6)
-        db['R1_path'] = fastq_path + db.Sample_Name + '_R1.fastq.gz'
-        db['R2_path'] = fastq_path + db.Sample_Name + '_R2.fastq.gz'
+        db['Sample_Name'] = 'PAFTOL_' + db['idSequence'].astype(int).astype('str').str.zfill(6)
+        fastq_suffix = "R"
     elif DataSource == 'GAP':
-        fastq_path = '/science/projects/paftol/AllData_symlinks/'
-        db['Sample_Name'] = 'GAP_' + db['idSequencing'].astype(int).astype('str').str.zfill(6)
-        db['R1_path'] = fastq_path + db.Sample_Name + '_R1.fastq.gz'
-        db['R2_path'] = fastq_path + db.Sample_Name + '_R2.fastq.gz'
+        db['Sample_Name'] = 'GAP_' + db['idSequence'].astype(int).astype('str').str.zfill(6)
+        fastq_suffix = "R"
     elif DataSource == 'SRA':
-        # Paul B. - modified path to process SRA data from these subsets: paftol/SRA_from_ARZ/new_SRA_batch_2/SP014[678]
-        fastq_path = '/data/projects/paftol/SRA_Data/'
-        #fastq_path = '/data/projects/paftol/new_data_ARZ_Jan22/SP0147/'
-        db['Sample_Name'] = db.ExternalSequenceID
-        db['R1_path'] = fastq_path + db.R1FastqFile
-        db['R2_path'] = fastq_path + db.R2FastqFile
+        db['Sample_Name'] = db['ExternalSequenceID']
+        fastq_suffix = ""
     else:
         print('unknown action for',DataSource)
         sys.exit()
+    db["R1_path"] = db['Sample_Name'].apply(lambda x: fastq_path / f"{x}_{fastq_suffix}1.fastq.gz")
+    db["R2_path"] = db['Sample_Name'].apply(lambda x: fastq_path / f"{x}_{fastq_suffix}2.fastq.gz")
     print(db.shape[0],'samples in total')
     return db
 
